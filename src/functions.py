5ebcbc18d5785bdc8818e1726c0051fb4f758827
ottwil01 <ottwil01@gmail.com>
2022-09-29 16:29:57 +1000

    fixed table

---
+++ src/functions.py
@@ -0,0 +1,7 @@
+def f_to_c(fahrenheit):
+    celcius = (fahrenheit - 32) * (5 / 9)
+    return celcius
+
+def meter_to_feet(meter):
+    feet = meter * 3.28
+    return feet
---
+++ src/main.py
@@ -0,0 +1,84 @@
+import datetime as dt
+from datetime import date
+import requests
+from simple_term_menu import TerminalMenu
+from prettytable import PrettyTable
+import functions
+
+
+base_url = 'http://api.openweathermap.org/data/2.5/weather?'
+api_key = 'f820095e8faeb3eb39572008bae7a59c'
+city = input('Which city in Australia are you flying out of? ')
+url = base_url + '&appid=' + api_key + '&q=' + city + '&units=imperial'
+response = requests.get(url).json()
+
+if response['cod'] == '404':
+    city = input('That city is not available, please try again: ')
+    url = base_url + '&appid=' + api_key + '&q=' + city + '&units=imperial'
+    response = requests.get(url).json()
+
+
+
+ground_temp = functions.f_to_c(response['main']['temp'])
+weather_description = response['weather'][0]['description']
+current_humidity = response['main']['humidity']
+cloud_cover_percent = (response['clouds']['all'])
+dew_point = ground_temp - ((100 - current_humidity) / 5)
+cloud_base = ((ground_temp - dew_point) / 2.5) * 1000
+cloud_base_temp = (-0.00984 * cloud_base) + ground_temp
+alt_clear = "{:.2f}".format((10 + ground_temp) / 0.00984)
+alt_mixed = "{:.2f}".format((15 + ground_temp) / 0.00984)
+alt_rime = "{:.2f}".format((20 + ground_temp) / 0.00984)
+
+flying_altitude = 2500
+
+
+def ice_type():
+    if weather_description != "clear sky" and cloud_base_temp <= 0:
+        if cloud_base <= flying_altitude and flying_altitude < alt_clear:
+            hazard = 'Clear Ice'
+            print(hazard)
+        elif alt_clear <= flying_altitude < alt_mixed:
+            hazard = 'Mixed Ice'
+            print(hazard)
+        elif alt_mixed <= flying_altitude < alt_rime:
+            hazard = 'Rime Ice'
+            print(hazard)
+
+
+if weather_description != "clear sky" and cloud_base_temp <= 0:
+    ice_present = 'Yes'
+else:
+    ice_present = 'No'
+
+
+print(ground_temp, cloud_base_temp, cloud_base, alt_clear, alt_mixed, alt_rime)
+
+x = PrettyTable()
+x.field_names = ['City Name', 'Weather Description', 'Cloud Cover (%)', 'Cloud Base (ft)', 'Ice Present', 'Ice-free Flying Altitude']
+x.add_row([city, weather_description, cloud_cover_percent, cloud_base, ice_present, f"It is safe to fly below {cloud_base}, or above {alt_rime}"])
+print(x)
+
+
+
+fly_today = input('Are you happy to fly today? (yes/no) ')
+if fly_today == 'yes' and ice_present == 'yes':
+    print('Enjoy your flight, but watch out for ice!')
+    print('')
+elif fly_today == 'yes' and ice_present == 'no':
+    print('Enjoy your flight, the weather appears to be clear!')
+elif fly_today == 'no':
+    def day():
+        print("This is the forecast for the next week, when would you like to fly?")
+        options = [str(dt.date.today() + dt.timedelta(days=1)), str(dt.date.today() + dt.timedelta(days=2)), str(dt.date.today() + dt.timedelta(days=3)), str(dt.date.today() + dt.timedelta(days=4)), str(dt.date.today() + dt.timedelta(days=5)), str(dt.date.today() + dt.timedelta(days=6)), str(dt.date.today() + dt.timedelta(days=7))]
+        terminal_menu = TerminalMenu(options)
+        menu_entry_index = terminal_menu.show()
+        print(f"You have selected {options[menu_entry_index]}!")
+    if __name__ == "__day__":
+        day()
+
+# forecast_url = 'http://api.openweathermap.org/data/2.5/forecast?'
+# api_key = 'f820095e8faeb3eb39572008bae7a59c'
+# url = forecast_url + '&appid=' + api_key + '&q=' + city + '&units=imperial'
+# response = requests.get(url).json()
+# print(response)
